d38b42da0239e38bef92fb6fe8738dedf47cb795
CocoaCoding <holgerhinzberg@holger-hinzbergs-imac-5.fritz.box>
2017-01-14 20:42:33 +0100

    Initial Commit

---
+++ PictureDesk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PictureDesk
+//
+//  Created by Holger Hinzberg on 14.01.17.
+//  Copyright © 2017 Holger Hinzberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PictureDesk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PictureDesk
+//
+//  Created by Holger Hinzberg on 14.01.17.
+//  Copyright © 2017 Holger Hinzberg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
